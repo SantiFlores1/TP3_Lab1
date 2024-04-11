a901b8293b13f37f2d473b622c4f2c6a949bdc9c
Iara Nievas <iarafuentesok@hotmail.com>
2024-04-11 20:51:17 -0300

    clase principal
    Agregue una clase principal para llamar las vistas

---
+++ src/logica/Principal.java
@@ -0,0 +1,14 @@
+
+package logica;
+
+import tp3lab1.FormLogin;
+
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        FormLogin log=new FormLogin();
+        log.setVisible(true);
+        log.setLocationRelativeTo(null);
+    }
+}
